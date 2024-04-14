54de306bd41454a4684eb3e085903104439fa074
NoobExploits <142132589+NoobExploits@users.noreply.github.com>
2024-04-13 23:07:14 -0500

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,7 +1,7 @@
 -- Got from https://github.com/RegularVynixu/Vynixius/blob/main/Loader.lua btw :)
 local games = {
-    [{155615604}] = "https://raw.githubusercontent.com/NoobExploits/Impact/main/games/Prison%20Life.lua",
-    [{662417684}] = "https://raw.githubusercontent.com/NoobExploits/Impact/main/games/Lucky%20Block%20BG.lua",
+    [{155615604}] = "https://raw.githubusercontent.com/NoobExploits/Impact/main/games/Prison%20Life/PC.lua",
+    [{662417684}] = "https://raw.githubusercontent.com/NoobExploits/Impact/main/games/Lucky%20Block%20BG/PC.lua",
 }
 
 for ids, url in next, games do
