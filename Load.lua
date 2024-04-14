8cc9bac7b8d461424ec3f2531a10f192ac742846
NoobExploits <142132589+NoobExploits@users.noreply.github.com>
2024-04-14 02:09:54 -0500

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,12 +1,33 @@
--- Got from https://github.com/RegularVynixu/Vynixius/blob/main/Loader.lua btw :)
-local games = {
-    [{155615604}] = "https://raw.githubusercontent.com/NoobExploits/Impact/main/games/Prison%20Life/PC.lua",
-    [{662417684}] = "https://raw.githubusercontent.com/NoobExploits/Impact/main/games/Lucky%20Block%20BG/PC.lua",
-}
+local flu = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local Inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
 
-for ids, url in next, games do
-    if table.find(ids, game.PlaceId) then
-        loadstring(game:HttpGet(url))()
-        break
+flu:Notify(
+    {
+        Title = "Please Wait.  📢",
+        Content = "Impact is loading, please give us a second!",
+        Duration = 5
+    }
+)
+
+local succ, err =
+    xpcall(
+    function()
+        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoobExploits/Impact/main/src/Checker.lua", true))()
+
+        print("Impact Output: Loaded with 0 issues.  💫")
+    end,
+    function(err)
+        if (string.find(err, "404")) or (string.find(err, "attempt to call a nil value")) then
+            flu:Notify(
+                {
+                    Title = "Impact failed to load!  💫",
+                    Content = "The script is currently down or something broke, please try again later or message @noobexploits.",
+                    Duration = 5
+                }
+            )
+
+            Inviter.Prompt({name = "Impact", invite = "discord.gg/PDxxpQ3nqm"})
+            Inviter.Join("discord.gg/PDxxpQ3nqm")
+        end
     end
-end
+)
