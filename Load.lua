40295fa69380aa8ae62a3ebbb04c5a41192e85f4
NoobExploits <142132589+NoobExploits@users.noreply.github.com>
2024-04-14 19:41:53 -0500

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,34 +1,24 @@
 local flu = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local Inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
 
+flu:Notify({
+    Title = "Please Wait.  📢",
+    Content = "Impact is loading, please give us a second!",
+    Duration = 5
+})
 
-flu:Notify(
-    {
-        Title = "Please Wait.  📢",
-        Content = "Impact is loading, please give us a second!",
-        Duration = 5
-    }
-)
+local succ, err = xpcall(function()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoobExploits/Impact/main/src/Checker.lua", true))()
 
-local succ, err =
-    xpcall(
-    function()
-        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoobExploits/Impact/main/src/Checker.lua", true))()
-        
-        print("Impact Output: Loaded with 0 issues.  💫")
-    end,
-    function(err)
-        if (string.find(err, "404")) or (string.find(err, "attempt to call a nil value")) then
-            flu:Notify(
-                {
-                    Title = "Impact failed to load!  💫",
-                    Content = "The script is currently down or something broke, please try again later or message @noobexploits.",
-                    Duration = 5
-                }
-            )
+    print("Impact Output: Loaded with 0 issues.  💫")
+end, function(err)
+    if (string.find(err, "404")) or (string.find(err, "attempt to call a nil value")) then
+        flu:Notify({
+            Title = "Impact failed to load!  💫",
+            Content = "The script is currently down or something broke, please try again later or message @noobexploits.",
+            Duration = 5
+        })
 
-            Inviter.Prompt({name = "Impact", invite = "discord.gg/PDxxpQ3nqm"})
-            Inviter.Join("discord.gg/PDxxpQ3nqm")
-        end
+        Inviter.Prompt({name = "Impact", invite = "discord.gg/PDxxpQ3nqm"})
     end
-)
+end)
