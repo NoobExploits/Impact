4c4df68b7d4cdcb0475710f96b2ef16ef3e60b63
NoobExploits <142132589+NoobExploits@users.noreply.github.com>
2024-04-17 18:47:32 -0500

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -2,7 +2,7 @@ local flu = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/rel
 
 if (identifyexecutor()) == "Delta" then
       flu:Notify({
-        Title = "Delta Detected  ☢️",
+        Title = "☢️  Delta Detected",
         Content = "This script has some features that Delta doesnt have please switch to something else! (Example: Codex)",
         Duration = 999999
     })
@@ -13,7 +13,7 @@ end
 
 flu:Notify(
     {
-        Title = "Please Wait.  📢",
+        Title = "📢  Please Wait.",
         Content = "Impact is loading, please give us a second!",
         Duration = 5
     }
@@ -26,13 +26,13 @@ local succ, err =
             game:HttpGetAsync("https://raw.githubusercontent.com/NoobExploits/Impact/main/src/Checker.lua", true)
         )()
 
-        print("Impact Output: Loaded with 0 issues.  💫")
+        print("🚀 Impact Output: Loaded with 0 issues.")
     end,
     function(err)
         if (string.find(err, "404")) or (string.find(err, "attempt to call a nil value")) then
             flu:Notify(
                 {
-                    Title = "Impact failed to load!  💫",
+                    Title = "💫  Impact failed to load!",
                     Content = "The script is currently down or something broke, please try again later or message @noobexploits.",
                     Duration = 5
                 }
