cbfaf00c3d50879d0cd5e04403136cce820fc697
NoobExploits <142132589+NoobExploits@users.noreply.github.com>
2024-04-17 19:23:52 -0500

    Create PC.lua

---
+++ games/Universal/PC.lua
@@ -0,0 +1,9 @@
+local flu = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+
+flu:Notify(
+    {
+        Title = "☢️  Universal Not Added",
+        Content = "Universal isnt supported yet, please use the supported games!",
+        Duration = 5
+    }
+)
