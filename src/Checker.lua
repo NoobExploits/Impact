345eb1a504ce4498631b2c8ef181850a60338258
NoobExploits <142132589+NoobExploits@users.noreply.github.com>
2024-04-17 19:19:29 -0500

    this prob works x2 (added universal soon)

--- src/Checker.lua
+++ src/Checker.lua
@@ -2,6 +2,7 @@ local devices = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/
 
 local placeid = game.PlaceId
 
+-- Checks if it got Windows or Mobile from the device checker
 local function isMobile()
     if (devices) == "Windows" or (string.find(devices, "Krampus")) then
         print("🚀 Impact Output: PC  💻")
@@ -17,10 +18,20 @@ local games = {
     [662417684] = "https://raw.githubusercontent.com/NoobExploits/Impact/main/games/Lucky%20Block%20BG/PC.lua",
 }
 
+local found = false
+
+-- Load Script From Games Table
 for ids, url in next, games do
     if ids == game.PlaceId then
+        found = true
         loadstring(game:HttpGet(url))()
         break
     end
 end
 
+-- Universal Script
+if not found then
+    found = false -- this isnt even needed but i did anyways so HA
+    local url = "https://raw.githubusercontent.com/NoobExploits/Impact/main/games/Universal/" .. isMobile() ..".lua",
+    loadstring(game:HttpGet(url))()
+end
