e2ccd1086f23451581683ac63b74bfcfda4f8cd2
NoobExploits <142132589+NoobExploits@users.noreply.github.com>
2024-04-28 20:03:12 -0500

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,48 +1,21 @@
 local flu = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
-if (identifyexecutor()) == "Delta" then
-      flu:Notify({
-        Title = "☢️  Delta Detected",
-        Content = "This script has some features that Delta doesnt have please switch to something else! (Example: Codex)",
-        Duration = 999999
-    })
-    setclipboard("discord.gg/PDxxpQ3nqm")
-    wait(15)
-    game.Players.LocalPlayer:Kick("Discord Server Copied To Clipboard")
-end
+flu:Notify({
+    Title = "📢  Please Wait.",
+    Content = "Impact is currently loading.",
+    Duration = 5
+})
 
-flu:Notify(
-    {
-        Title = "📢  Please Wait.",
-        Content = "Impact is loading, please give us a second!",
-        Duration = 5
-    }
-)
+local succ, err = xpcall(function()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoobExploits/Impact/main/src/Checker.lua", true))()
 
-local succ, err =
-    xpcall(
-    function()
-        loadstring(
-            game:HttpGetAsync("https://raw.githubusercontent.com/NoobExploits/Impact/main/src/Checker.lua", true)
-        )()
-
-        print("🚀 Impact Output: Loaded with 0 issues.")
-    end,
-    function(err)
-        if (string.find(err, "404")) or (string.find(err, "attempt to call a nil value")) then
-            flu:Notify(
-                {
-                    Title = "💫  Impact failed to load!",
-                    Content = "The script is currently down or something broke, please try again later or message @noobexploits.",
-                    Duration = 5
-                }
-            )
-
-
-            
-            if (setclipboard) then
-                setclipboard("discord.gg/PDxxpQ3nqm")
-            end
-        end
+    print("🚀  Impact Output: Loaded with 0 issues.")
+end, function(err)
+    if (string.find(err, "404")) or (string.find(err, "attempt to call a nil value")) then
+        flu:Notify({
+            Title = "🚀  Impact Can't Start.",
+            Content = "The script is currently down, please try again later.",
+            Duration = 5
+        })
     end
-)
+end)
