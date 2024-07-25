9e3ce927db42b6f49aa4e12a9281e5846e28ef7f
NoobExploits <142132589+NoobExploits@users.noreply.github.com>
2024-07-25 02:52:28 -0500

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,10 +1,10 @@
-local flu = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+_Hawk = "ohhahtuhthttouttpwuttuaunbotwo"
+local Hawk = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheHanki/HawkHUB/main/LibSources/HawkLib.lua", true))()
+local discordInviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
 
-flu:Notify({
-    Title = "📢  Please Wait.",
-    Content = "Impact is currently loading.",
-    Duration = 5
-})
+local Notifications = Hawk:AddNotifications()
+
+Notifications:Notification("📢  Please Wait.","Impact is currently loading.","Notify",5)
 
 local succ, err = xpcall(function()
     loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoobExploits/Impact/main/src/Checker.lua", true))()
@@ -12,10 +12,11 @@ local succ, err = xpcall(function()
     print("🚀  Impact Output: Loaded with 0 issues.")
 end, function(err)
     if (string.find(err, "404")) or (string.find(err, "attempt to call a nil value")) then
-        flu:Notify({
-            Title = "🚀  Impact Can't Start.",
-            Content = "The script is currently down, please try again later.",
-            Duration = 5
+        discordInviter.Prompt({
+            name = "Impact",
+            invite = "https://discord.gg/sGUxgb7jcH"
         })
+        
+        Notifications:Notification("🚀  Impact Can't Start.","The script is currently down, please try again later. Discord: @noobexploits","Error",5)
     end
 end)
